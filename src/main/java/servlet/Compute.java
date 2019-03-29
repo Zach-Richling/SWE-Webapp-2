5b5986499b3d193483072738d22cfda540c68f3b
Zach Richling <zwrdude@gmail.com>
2019-03-29 17:36:43 -0500

    Update Compute.java

--- src/main/java/servlet/Compute.java
+++ src/main/java/servlet/Compute.java
@@ -22,56 +22,7 @@ public class Compute extends HttpServlet {
 		}
 		String htmlServlet = "";
 		int col = 2;
-		htmlServlet = htmlServlet + "<!DOCTYPE html>\n" + "<html lang='en'>\n" + "  <head>\n"
-				+ "    <meta charset='UTF-8'>\n" + "    <title>Compute</title>\n" + "    <style>\n"
-				+ "      table, th, td { border:1px solid black;\n" + "                      margin:10px;\n"
-				+ "                      padding:10px 30px 10px 30px; }\n"
-				+ "      .inline_wide { display: inline-block; width:100%; }\n" + "      body { text-align: center; }\n"
-				+ "      body > * { text-align: left; }\n" + "      form { display: inline-block; }\n"
-				+ "    </style>\n" + "  </head>\n" + "  <body>\n" + "    <form>\n" + "		<hr>\n" + "      <table>\n"
-				+ "        <tr>\n" + "          <td>First Number </td>\n" + "          <td>Second Number</td>\n";
-		if (sum != null) {
-			if (sum.equals("true")) {
-				htmlServlet = htmlServlet + "<td> Sum </td>";
-				col++;
-			}
-		}
-		if (difference != null) {
-			if (difference.equals("true")) {
-				htmlServlet = htmlServlet + "<td> Difference </td>";
-				col++;
-			}
-		}
-		if (product != null) {
-			if (product.equals("true")) {
-				htmlServlet = htmlServlet + "<td> Product </td>";
-				col++;
-			}
-		}
-		htmlServlet = htmlServlet + "        </tr>\n" + "        <tr>\n" + "          <td>" + firstNumber + "</td>\n"
-				+ "          <td>" + secondNumber + "</td>\n";
-		if (sum != null) {
-			if (sum.equals("true")) {
-				htmlServlet = htmlServlet + "<td>" + (Integer.parseInt(firstNumber) + Integer.parseInt(secondNumber))
-						+ "</td>";
-			}
-		}
-		if (difference != null) {
-			if (difference.equals("true")) {
-				htmlServlet = htmlServlet + "<td>" + (Integer.parseInt(firstNumber) - Integer.parseInt(secondNumber))
-						+ "</td>";
-			}
-		}
-		if (product != null) {
-			if (product.equals("true")) {
-				htmlServlet = htmlServlet + "<td>" + (Integer.parseInt(firstNumber) * Integer.parseInt(secondNumber))
-						+ "</td>";
-			}
-		}
-		htmlServlet = htmlServlet + "        </tr>\n" + "      </table>\n" + "		<hr>\n"
-				+ "		<button type='submit' style='display: block; margin: 0 auto;'\n"
-				+ "                    formaction='index.html'>\n" + "                    Return to Homepage</button>\n"
-				+ "    </form>\n" + "  </body>\n" + "</html>\n";
+		htmlServlet = htmlServlet + "<!DOCTYPE html> <html> <head> <meta charset="UTF-8"> <title>title</title> </head> <body> </body> </html>";
 		out.print(htmlServlet);
 		out.close();
 		return;
