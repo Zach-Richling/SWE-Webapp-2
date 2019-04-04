ce38b36f4c39d2c7910cad01586104720a9db0d3
Zach Richling <zwrdude@gmail.com>
2019-04-04 12:37:10 -0500

    Update WeatherAPICall.java

--- src/main/java/servlet/WeatherAPICall.java
+++ src/main/java/servlet/WeatherAPICall.java
@@ -11,7 +11,8 @@ import org.json.*;
 public class WeatherAPICall {
 
 	private static HttpURLConnection con;
-	private JSONObject json;
+	public JSONObject json;
+	
 	public WeatherAPICall(String zipcode) throws MalformedURLException, ProtocolException, IOException {
 		String url = "http://api.openweathermap.org/data/2.5/forecast?zip=" + zipcode
 				+ ",us&appid=8080d1949b56c215e309570924559b1e&units=imperial";
@@ -37,18 +38,31 @@ public class WeatherAPICall {
 			con.disconnect();
 		}
 	}
+	// Each day has 8 entries in the JSON Object. Each represents a 3 hour span on that day. Day 1 is : 0-7, Day 2 is: 8-16, Etc.
 	
+	// Returns the date at a certain index in UTC.
 	public String getDateAtIndex(int index) {
 		String date = "default";
 		try {
-			date = (((JSONObject)(((JSONArray)(json.get("list"))).get(0))).get("dt")).toString();
+			date = (((JSONObject)(((JSONArray)(json.get("list"))).get(index))).get("dt")).toString();
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return date;
 	}
 	
+	// Returns the date at a certain index in this format: Year-Month-Day Time
+	public String getDateTextAtIndex(int index) {
+		String date = "default";
+		try {
+			date = (((JSONObject)(((JSONArray)(json.get("list"))).get(index))).get("dt_txt")).toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return date;
+	}
+	
+	// Returns the average temperature for a day.
 	public String getTempAtIndex(int index) {
 		String temp = "";
 		try {
@@ -59,10 +73,33 @@ public class WeatherAPICall {
 		return temp;
 	}
 	
+	// Returns the humidity for a day.
 	public String getHumidityAtIndex(int index) {
 		String temp = "";
 		try {
 			temp = ((JSONObject)((JSONObject)((JSONArray)json.get("list")).get(index)).get("main")).get("humidity").toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return temp;
+	}
+	
+	// Returns the description of the weather, such as "light rain".
+	public String getDescriptionAtIndex(int index) {
+		String temp = "";
+		try {
+			temp = ((JSONObject)((JSONArray)((JSONObject)((JSONArray)json.get("list")).get(index)).get("weather")).get(0)).get("description").toString();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return temp;
+	}
+	
+	// Returns the wind speed in miles per hour.
+	public String getWindSpeedAtIndex(int index) {
+		String temp = "";
+		try {
+			temp = ((JSONObject)((JSONObject)((JSONArray)json.get("list")).get(index)).get("wind")).get("speed").toString();
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
