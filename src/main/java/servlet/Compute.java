e48a946276521f8db54258281a55502bb1754e12
Zach Richling <zwrdude@gmail.com>
2019-04-09 12:09:17 -0500

    Update Compute.java

--- src/main/java/servlet/Compute.java
+++ src/main/java/servlet/Compute.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.*;
 public class Compute extends HttpServlet {
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		WeatherAPICall data = new WeatherAPICall(request.getParameter("zipcode"));
+		WeatherAPICall data = new WeatherAPICall(request.getParameter("ZipCode"));
 		response.setContentType("text/html");
 		final PrintWriter out = response.getWriter();
 		Cookie[] cookieArray = request.getCookies();
