95be83884faedceeea7753324d2175e851ddd387
Zach Richling <zwrdude@gmail.com>
2019-04-11 17:59:19 -0500

    Update Compute.java

--- src/main/java/servlet/Compute.java
+++ src/main/java/servlet/Compute.java
@@ -21,7 +21,7 @@ public class Compute extends HttpServlet {
 		}
 		checkValue = cookieArray[0].getValue();
 		*/
-		checkValue = data.getSnow(0);
+		checkValue = data.getSnow(1);
 		String htmlServlet = "";
 		htmlServlet = htmlServlet + 
 		"<!DOCTYPE html>" +
