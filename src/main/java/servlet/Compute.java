7bac3488b5941b6212051ec7d7e479a20ef69a11
dsnyder96 <46458387+dsnyder96@users.noreply.github.com>
2019-04-15 17:57:57 -0500

    Preferences Work

--- src/main/java/servlet/Compute.java
+++ src/main/java/servlet/Compute.java
@@ -8,19 +8,28 @@ import org.apache.commons.lang3.*;
 @WebServlet(name = "Compute", urlPatterns = { "/Compute" })
 public class Compute extends HttpServlet {
 	
-
+		String highLow;
+		String cloud;
+		String pressure;
+		String avgtemp;
+		String windSpeed;
+		String rain;
+		String snow;
+		String humidity;
+		
 	@Override
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		WeatherAPICall data = new WeatherAPICall(request.getParameter("ZipCode"));
+		int[] pref = new int[8];
 		String zip = request.getParameter("ZipCode");
-		String highLow = request.getParameter("HighLow");
-		String cloud = request.getParameter("CloudCover");
-		String pressure = request.getParameter("Pressure");
-		String avgtemp = request.getParameter("Avg");
-		String windSpeed = request.getParameter("WindSpeed");
-		String rain = request.getParameter("Rain");
-		String snow = request.getParameter("Snow");
-		String humidity = request.getParameter("Humidity");
+		highLow = request.getParameter("HighLow");
+		cloud = request.getParameter("CloudCover");
+		pressure = request.getParameter("Pressure");
+		avgtemp = request.getParameter("Avg");
+		windSpeed = request.getParameter("WindSpeed");
+		rain = request.getParameter("Rain");
+		snow = request.getParameter("Snow");
+		humidity = request.getParameter("Humidity");
 		
 		response.setContentType("text/html");
 		final PrintWriter out = response.getWriter();
@@ -124,11 +133,18 @@ public class Compute extends HttpServlet {
 		"	color: white;" +
 		"}" +
 		"#dataContainer {" +
+		"	display: block;" +
+		"	overflow: auto;" +
 		"	margin-top = 0;" +
 		"	height: 100%;" +
 		"	width: 100%;" +
 		"}" +
+		"td {" +
+		"	text-align: center;" +
+		"}" +
 		".periodData{" +
+		"	display: block;" +
+		"	overflow: auto;" +
 		"	background-image: linear-gradient(to bottom right, #99c0ff, #fff38e);" +
 		"	width: 90%;" +
 		"	height: 115px;" +
@@ -143,51 +159,135 @@ public class Compute extends HttpServlet {
 		"</style>" +
 		"</head>" + 
 		"<body>" + 
-		"<p> Hello user, </p>" + 
-		"<p> Zipcode: " + zip + " Preferences Set: highLow: " + highLow + "; cloud: " + cloud + "; pressure: " + pressure + "; avgtemp: " + avgtemp + "; windSpeed: " + windSpeed + "; rain: " + rain + "; snow: " + snow + "; humidity: " + humidity + "</p>" +
-		getDataForFullDay(1,data) +
-		getDataForFullDay(2,data) +
+		"<div class=\"topnav\" id=\"topNav\">" +
+		"<a class=\"active\" href=\"index.html\"><b>Home</b></a>" +
+		"<a href=\"javascript:void(0);\" class=\"icon\" onclick=\"myFunction()\">" +
+		"	<i class=\"fa fa-bars\"></i>" +
+		"</a>" +
+	"</div>" +
+	"<div id=\"dataContainer\" style=\"padding-left:16px\">" +
+	"	<div class=\"periodData\">" +
+			getDataForFullDay(1,data,pref) +
+	"	</div>" +
+	"	<div class=\"periodData\">" +
+			getDataForFullDay(2,data,pref) +
+	"	</div>" +
+		"</div>" +
+		"<div id=\"formContainer\">" +
+		"	<form action=\"Compute\" method=\"POST\">" +
+		"		<input type=\"Numeric\" name=\"ZipCode\" placeholder=\"Enter zipcode...\" maxlength=\"5\" required><br><br>" +
+		"		<input type=\"checkbox\" name=\"CloudCover\" value=\"cloud\">Cloud Coverage</br>" +
+		"		<input type=\"checkbox\" name=\"Pressure\" value=\"pressure\">Pressure</br>" +
+		"		<input type=\"checkbox\" name=\"Avg\" value=\"avg\">Average Temperature</br>" +
+		"		<input type=\"checkbox\" name=\"HighLow\" value=\"highlow\">High/Low</br>" +
+		"		<input type=\"checkbox\" name=\"WindSpeed\" value=\"windspeed\">Wind Speed</br>" +
+		"		<input type=\"checkbox\" name=\"Rain\" value=\"rain\">Rain</br>" +
+		"		<input type=\"checkbox\" name=\"Snow\" value=\"snow\">Snow</br>" +
+		"		<input type=\"checkbox\" name=\"Humidity\" value=\"humidity\">Humidity</br>" +
+		"		<br><input type=\"submit\" class='submitPref' value=\"Update Preferences\">" +
+		"	</form>" +
+		"</div>" +
+		"<script>" +
+		"document.getElementById(\"formContainer\").style.display = \"none\";" +
+		"//document.getElementById(\"dataContainer\").style.display = \"none\";" +
+		"function myFunction() {" +
+		"	var x = document.getElementById(\"topNav\");" +
+		"	if (x.className === \"topnav\") {" +
+		"		x.className += \" responsive\";" +
+		"	} else {" +
+		"		x.className = \"topnav\";" +
+		"	}" +
+		"}" +
+		"function toggleDiv(id){" +
+		"	var toHide = document.getElementById(id);" +
+		"	if(toHide.style.display == null || toHide.style.display == \"none\"){" +
+		"		toHide.style.display = \"block\";" +
+		"	}" +
+		"	else{" +
+		"		toHide.style.display = \"none\";" +
+		"	}" +
+		"}" +
+		"function goHome(){" +
+		"	if(document.getElementById(\"formContainer\").style.display == \"block\")" +
+		"		toggleDiv(\"formContainer\");" +
+		"	if(document.getElementById(\"dataContainer\").style.display == \"block\")" +
+		"		toggleDiv(\"dataContainer\");" +
+		"}" +
+		"</script>" +
 		"</body>" + 
 		"</html>";
 		out.print(htmlServlet);
 		out.close();
+		
 		return;
 	} // end doPost method
 	
 	// return html for 24hr period 
 	// Date refers to date on the start of the 24hr period
-	public String getDataForFullDay(int day, WeatherAPICall data) {
+	public String getDataForFullDay(int day, WeatherAPICall data, int[] pref) {
 		int dayStart = day * 8;
+		
+		String toReturn = "";
+
+		if(!(avgtemp == null)){toReturn += "<td>Avg Temp</td>";}
+		if(!(highLow == null)){toReturn += "<td>High/Low</td>";}
+		if(!(humidity == null)){toReturn += "<td>Humidity</td>";}
+		if(!(cloud == null)){toReturn += "<td>Description</td>";}
+		if(!(windSpeed == null)){toReturn += "<td>Wind Speed</td>";}
+		if(!(rain == null)){toReturn += "<td>Rain</td>";}
+		if(!(snow == null)){toReturn += "<td>Snow</td>";}
+		if(!(pressure == null)){toReturn += "<td>Pressure</td>";}
+		
 		return
 		"<table>"+
 		"<th colspan=\"9\"> Date: " + data.date(data.getDateTextAtIndex(dayStart)) + "</th>" +
-		"<tr><td>Time</td><td>Avg Temp</td><td>High/Low</td><td>Humidity</td>"+
-		"<td>Description</td><td>Wind Speed</td><td>Rain</td><td>Snow</td><td>Pressure</td></tr>"+
-		getDataForIndex(dayStart + 1,data)+
-		getDataForIndex(dayStart + 2,data)+
-		getDataForIndex(dayStart + 3,data)+
-		getDataForIndex(dayStart + 4,data)+
-		getDataForIndex(dayStart + 5,data)+
-		getDataForIndex(dayStart + 6,data)+
-		getDataForIndex(dayStart + 7,data)+
-		getDataForIndex(dayStart + 8,data)+
+		"<tr><td>Time</td>"+
+		toReturn +
+		"</tr>"+
+		getDataForIndex(dayStart + 1,data,pref)+
+		getDataForIndex(dayStart + 2,data,pref)+
+		getDataForIndex(dayStart + 3,data,pref)+
+		getDataForIndex(dayStart + 4,data,pref)+
+		getDataForIndex(dayStart + 5,data,pref)+
+		getDataForIndex(dayStart + 6,data,pref)+
+		getDataForIndex(dayStart + 7,data,pref)+
+		getDataForIndex(dayStart + 8,data,pref)+
 		"</table>";
 	}
 	
 	// 
-	public String getDataForIndex(int index, WeatherAPICall data) {
+	public String getDataForIndex(int index, WeatherAPICall data, int[] pref) {
+		String toReturn = "";
 
+		if(!(avgtemp == null)){
+			toReturn += "<td>" + data.getTempAtIndex(index) + "</td>";
+		}
+		if(!(highLow == null)){
+			toReturn += "<td>" + data.getTempMaxAtIndex(index) + "/" + data.getTempMinAtIndex(index) + "</td>";
+		}
+		if(!(humidity == null)){
+			toReturn += "<td>" + data.getHumidityAtIndex(index) + "</td>";
+		}
+		if(!(cloud == null)){
+			toReturn += "<td>" + data.getDescriptionAtIndex(index) + "</td>";
+		}
+		if(!(windSpeed == null)){
+			toReturn += "<td>" + data.getWindSpeedAtIndex(index) + "</td>";
+		}
+		if(!(rain == null)){
+			toReturn += "<td>" + data.getRainAtIndex(index) + "</td>";
+		}
+		if(!(snow == null)){
+			toReturn += "<td>" + data.getSnowAtIndex(index) + "</td>";
+		}
+		if(!(pressure == null)){
+			toReturn += "<td>" + data.getPressureAtIndex(index) + "</td>";
+		}
+		
 		return
 		"<tr>"+
 		"<td>" + data.time(data.getDateTextAtIndex(index)) + "</td>" +
-		"<td>" + data.getTempAtIndex(index) + "</td>" +
-		"<td>" + data.getTempMaxAtIndex(index) + "/" + data.getTempMinAtIndex(index) + "</td>" +
-		"<td>" + data.getHumidityAtIndex(index) + "</td>" +
-		"<td>" + data.getDescriptionAtIndex(index) + "</td>" +
-		"<td>" + data.getWindSpeedAtIndex(index) + "</td>" +
-		"<td>" + data.getRainAtIndex(index) + "</td>" +
-		"<td>" + data.getSnowAtIndex(index) + "</td>" +
-		"<td>" + data.getPressureAtIndex(index) + "</td>"+
+		toReturn +
 		"</tr>";
 	}
 	
