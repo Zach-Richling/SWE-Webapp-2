cf0f4828841442b20a2cab4c9067340ffcedb8d9
dsnyder96 <46458387+dsnyder96@users.noreply.github.com>
2019-04-23 14:22:08 -0500

    Got rid of unused html doc weatherdata

--- src/main/java/servlet/Compute.java
+++ src/main/java/servlet/Compute.java
@@ -139,7 +139,7 @@ public class Compute extends HttpServlet {
 		"<title>WeatherForcast</title>" +
 		"<style>" +
 		"html {" +
-		"	height: 100%;" +	
+		"	height: auto;" +	
 		"}" +
 		"body {" +
 		"	margin: 0;" +
